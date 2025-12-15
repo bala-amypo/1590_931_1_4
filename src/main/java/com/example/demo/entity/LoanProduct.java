4fd5ecfb4e2d73a40b6f1599dd8887ba74196a52
bala-amypo <bala@amypo.in>
2025-12-15 16:41:54 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/LoanProduct.java
@@ -0,0 +1,12 @@
+package com.example.demo.entity;
+
+public class LoanProduct {
+
+    private long id;
+    private String productCode;
+    private String productName;
+    private String loanType;
+    
+
+    
+}
