05053d70ae01abb2264cd995c2615282cb3a0073
bala-amypo <bala@amypo.in>
2025-12-15 16:51:56 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/ApplicantProfile.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity;
+
+public class ApplicantProfile {
+
+    private long id;
+    private User user;
+    private String fullName;
+    private LocalDate dateOfBirth;
+    private 
+    
+}
--- src/main/java/com/example/demo/entity/LoanProduct.java
+++ src/main/java/com/example/demo/entity/LoanProduct.java
@@ -15,6 +15,102 @@ public class LoanProduct {
     private Double annualInterestRatePercent;
     private Integer baseRiskWeight;
 
+    public LoanProduct(){}
+
+    public LoanProduct(String productCode, String productName, String loanType, BigDecimal minAmount,
+            BigDecimal maxAmount, Integer minTenureMonths, Integer maxTenureMonths, Double annualInterestRatePercent,
+            Integer baseRiskWeight) {
+        this.productCode = productCode;
+        this.productName = productName;
+        this.loanType = loanType;
+        this.minAmount = minAmount;
+        this.maxAmount = maxAmount;
+        this.minTenureMonths = minTenureMonths;
+        this.maxTenureMonths = maxTenureMonths;
+        this.annualInterestRatePercent = annualInterestRatePercent;
+        this.baseRiskWeight = baseRiskWeight;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getLoanType() {
+        return loanType;
+    }
+
+    public void setLoanType(String loanType) {
+        this.loanType = loanType;
+    }
+
+    public BigDecimal getMinAmount() {
+        return minAmount;
+    }
+
+    public void setMinAmount(BigDecimal minAmount) {
+        this.minAmount = minAmount;
+    }
+
+    public BigDecimal getMaxAmount() {
+        return maxAmount;
+    }
+
+    public void setMaxAmount(BigDecimal maxAmount) {
+        this.maxAmount = maxAmount;
+    }
+
+    public Integer getMinTenureMonths() {
+        return minTenureMonths;
+    }
+
+    public void setMinTenureMonths(Integer minTenureMonths) {
+        this.minTenureMonths = minTenureMonths;
+    }
+
+    public Integer getMaxTenureMonths() {
+        return maxTenureMonths;
+    }
+
+    public void setMaxTenureMonths(Integer maxTenureMonths) {
+        this.maxTenureMonths = maxTenureMonths;
+    }
+
+    public Double getAnnualInterestRatePercent() {
+        return annualInterestRatePercent;
+    }
+
+    public void setAnnualInterestRatePercent(Double annualInterestRatePercent) {
+        this.annualInterestRatePercent = annualInterestRatePercent;
+    }
+
+    public Integer getBaseRiskWeight() {
+        return baseRiskWeight;
+    }
+
+    public void setBaseRiskWeight(Integer baseRiskWeight) {
+        this.baseRiskWeight = baseRiskWeight;
+    }
+
     
 
 
