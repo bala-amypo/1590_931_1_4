f48c74ea5cde8a45c11f86afaf5e235d962357c8
bala-amypo <bala@amypo.in>
2025-12-15 17:06:56 +0000

    User commit

--- src/main/java/com/example/demo/entity/LoanApplication.java
+++ src/main/java/com/example/demo/entity/LoanApplication.java
@@ -11,6 +11,119 @@ public class LoanApplication {
     private LocalDate applicationDate;
     private BigDecimal requestedAmount;
     private Integer requestedTenureMonths;
+    private String status;
+    private BigDecimal calculatedEmiAmount;
+    private String notes;
+
+    public LoanApplication(){}
     
+
+    public LoanApplication(ApplicantProfile applicant, LoanProduct loanProduct, LocalDate applicationDate,
+            BigDecimal requestedAmount, Integer requestedTenureMonths, String status, BigDecimal calculatedEmiAmount,
+            String notes) {
+        this.applicant = applicant;
+        this.loanProduct = loanProduct;
+        this.applicationDate = applicationDate;
+        this.requestedAmount = requestedAmount;
+        this.requestedTenureMonths = requestedTenureMonths;
+        this.status = status;
+        this.calculatedEmiAmount = calculatedEmiAmount;
+        this.notes = notes;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public ApplicantProfile getApplicant() {
+        return applicant;
+    }
+
+
+    public void setApplicant(ApplicantProfile applicant) {
+        this.applicant = applicant;
+    }
+
+
+    public LoanProduct getLoanProduct() {
+        return loanProduct;
+    }
+
+
+    public void setLoanProduct(LoanProduct loanProduct) {
+        this.loanProduct = loanProduct;
+    }
+
+
+    public LocalDate getApplicationDate() {
+        return applicationDate;
+    }
+
+
+    public void setApplicationDate(LocalDate applicationDate) {
+        this.applicationDate = applicationDate;
+    }
+
+
+    public BigDecimal getRequestedAmount() {
+        return requestedAmount;
+    }
+
+
+    public void setRequestedAmount(BigDecimal requestedAmount) {
+        this.requestedAmount = requestedAmount;
+    }
+
+
+    public Integer getRequestedTenureMonths() {
+        return requestedTenureMonths;
+    }
+
+
+    public void setRequestedTenureMonths(Integer requestedTenureMonths) {
+        this.requestedTenureMonths = requestedTenureMonths;
+    }
+
+
+    public String getStatus() {
+        return status;
+    }
+
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+    public BigDecimal getCalculatedEmiAmount() {
+        return calculatedEmiAmount;
+    }
+
+
+    public void setCalculatedEmiAmount(BigDecimal calculatedEmiAmount) {
+        this.calculatedEmiAmount = calculatedEmiAmount;
+    }
+
+
+    public String getNotes() {
+        return notes;
+    }
+
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
     
+
+
+
+
 }
---
+++ src/main/java/com/example/demo/entity/RiskAssessment.java
@@ -0,0 +1,7 @@
+package com.example.demo.entity;
+
+public class RiskAssessment {
+    
+    private long id;
+    private LoanApplication 
+}
