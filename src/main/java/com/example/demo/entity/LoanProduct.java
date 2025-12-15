c0ab26f1aaff494feb208c0bacb214bbab369dd8
bala-amypo <bala@amypo.in>
2025-12-15 16:46:54 +0000

    User commit

--- src/main/java/com/example/demo/entity/LoanProduct.java
+++ src/main/java/com/example/demo/entity/LoanProduct.java
@@ -1,12 +1,22 @@
 package com.example.demo.entity;
 
+import java.math.BigDecimal;
+
 public class LoanProduct {
 
     private long id;
     private String productCode;
     private String productName;
     private String loanType;
+    private BigDecimal minAmount;
+    private BigDecimal maxAmount;
+    private Integer minTenureMonths;
+    private Integer maxTenureMonths;
+    private Double annualInterestRatePercent;
+    private Integer baseRiskWeight;
+
     
+
 
     
 }
